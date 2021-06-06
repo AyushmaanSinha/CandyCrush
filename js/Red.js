c6fb2d2419be094bdb297ad62d18b52e60c483b4
Ayushmaan Sinha <70148474+AyushmaanSinha@users.noreply.github.com>
2021-06-06 23:36:24 +0530

    Add files via upload

---
+++ js/Red.js
@@ -0,0 +1,6 @@
+class Red extends BaseClass {
+    constructor(x,y){
+        super(x,y,50,50);
+        this.image = loadImage("images/Red.jpg")
+    }
+}
---
+++ js/Yellow.js
@@ -0,0 +1,6 @@
+class Yellow extends BaseClass {
+    constructor(x,y){
+        super(x,y,50,50);
+        this.image = loadImage("images/Yellow.jpg")
+    }
+}
